2197c974fa9ce37011ae37aad4ce36302d23de93
IsaacJ4 <isaac.james470@gmail.com>
2023-09-14 18:06:26 +1000

    connected database, used an SQL queuery to select data, changed table so its hopefully better.

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,25 @@
+import sqlite3
 import matplotlib.pyplot as plt
-        
+
+# connecting to the database
+conn = sqlite3.connect('climate.db')
+cursor = conn.cursor()
+
+#SQL query to select data from climate data base
+cursor.execute('SELECT * FROM ClimateData')
+data = cursor.fetchall()
+
+# close it
+conn.close()
+
 years = []
 co2 = []
 temp = []
+
+for row in data:
+    years.append(row[0])
+    co2.append(row[1])
+    temp.append(row[2])
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
@@ -13,6 +30,9 @@ plt.xlabel("Year (decade)")
 plt.subplot(2, 1, 2)
 plt.plot(years, temp, 'r*-') 
 plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
-plt.show() 
-plt.savefig("co2_temp_1.png") 
+plt.xlabel("Year (decade)")
+
+plt.tight_layout()
+plt.show()
+
+plt.savefig("co2_temp_1.png")
