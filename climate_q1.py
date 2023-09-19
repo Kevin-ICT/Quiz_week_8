21ff8eaa38c4e61f89e3f62cd1afa3a6615dfcb1
Aaron Horton <aaron.horton@mymomentum.org.au>
2023-09-19 21:32:04 +1000

    Created connection to SQL Database and read data from individual columns before placing in each list.

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,28 @@
 import matplotlib.pyplot as plt
+import sqlite3 as sql
         
 years = []
 co2 = []
-temp = []
+temps = []
+
+sqlConn = sql.connect('climate.db')
+cursor = sqlConn.cursor()
+
+queryYear = 'Select Year FROM ClimateData'
+queryco2 = 'Select co2 from ClimateData'
+querytemp = 'Select temp from ClimateData'
+
+cursor.execute(queryYear)
+year = cursor.fetchall()
+years = [i[0] for i in year]
+
+cursor.execute(queryco2)
+co = cursor.fetchall()
+co2 = years = [i[0] for i in co]
+
+cursor.execute(queryco2)
+temp = cursor.fetchall()
+temps = [i[0] for i in temp]
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
