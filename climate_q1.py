0949e7d78e7d5c3e0f74884eac8120b185c71067
sunwoonam <nswhelen@gmail.com>
2023-09-29 01:16:49 +1000

    Add code to fetch climate data

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,24 @@
+import sqlite3
+# Connect to the SQLite database
+conn = sqlite3.connect('climate.db')
+cursor = conn.cursor()
+
+# Fetch data from the "ClimateData" table and populate lists
+cursor.execute("SELECT Year, CO2, Temperature FROM ClimateData")
+data = cursor.fetchall()
+
+years = [row[0] for row in data]
+co2 = [row[1] for row in data]
+temp = [row[2] for row in data]
+
+conn.close()
+
+
 import matplotlib.pyplot as plt
-        
-years = []
-co2 = []
-temp = []
+
+# years = []
+# co2 = []
+# temp = []
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
